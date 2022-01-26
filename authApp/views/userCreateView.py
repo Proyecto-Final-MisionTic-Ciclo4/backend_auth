8c972d2f5a17b5e0281ce328172887f109101b37
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-01-26 10:03:05 -0500

    Se crean las vistas  del proyecto

---
+++ authApp/views/userCreateView.py
@@ -0,0 +1,39 @@
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.permissions import IsAuthenticated
+from authApp.serializers.userSerializer import UserSerializer
+
+
+
+"""from django.conf import settings
+from django.conf import settings
+from rest_framework_simplejwt.backends import TokenBackend
+"""
+
+""" Generics APIView"""
+
+class UserCreateView(views.APIView): 
+    
+    """queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer # la data la vamos a hacer con la data del body para crear el objeto de la categoria
+    # permission_class = [IsAuthenticated,]"""
+
+    def post(self, request, *args, **kwargs): # metodo HTTP (POST, GET, PUSH, DELETE) 
+                                    # recibe una data en el request del body 
+                                    # *args argumentos basicos del servicio web 
+                                    # **kwargs argumentos que se necesiten y que puedan llegar 
+    
+        serializer = UserSerializer(data=request.data) # Almacena los datos que vienen en el body de la peticion HTTP
+        serializer.is_valid(raise_exception=True) #Revisa los campos del serializador
+        serializer.save() # Llama la funcion create en el serializador
+        
+        token_data = {
+            'username': request.data['username'],
+            'password': request.data['password']
+        }
+        
+        token_serializer = TokenObtainPairSerializer(data=token_data)
+        token_serializer.is_valid(raise_exception=True)
+        return Response(token_serializer.valid_data, status=status.HTTP_201_CREATED)
+ 
---
+++ authApp/views/userDetailView.py
@@ -0,0 +1,26 @@
+from rest_framework import status, generic
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.backends import TokenBackend
+from django.conf import settings
+
+from authApp.models.user import User
+from authApp.serializers.userSerializer import UserSerializer
+
+
+class UserDetailView(generic.RetriveAPIView):
+    queryset = User.objects.all() # se traen todos los datos
+    serializer_class = UserSerializer # Me llama al to_representation para devolverme los datos
+    permission_classes = (IsAuthenticated, )
+    
+    def get(self, request, *args, **kwargs):
+        
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM']) # el algoritmo para encriptar
+        valid_data = tokenBackend.decode(token,verify=False)
+
+        if valid_data['user_id'] != kwargs['pk']: # En user_id se guarda el user que se esta logeando y lo compara con el usuario que viaja en la URL
+            stringResponse = {'detail':'Unauthorized Request'} # valida que el usuario este pidiendo info de el y no de otro user
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+                
+        return super().get(request, *args, **kwargs)
